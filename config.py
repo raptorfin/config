524de2584d2055dbef47f9a4bbd7895ade59f4c5
raptorfin <dsgreene713@me.com>
2012-11-17 14:14:41 -0500

    added logging

--- config.py
+++ config.py
@@ -1,16 +1,30 @@
+"""
+Configuration class which provides validation various types of configfiles.
+Caller can specify both default values as well as required values.
+Provides a base implementation for file formats
+"""
+
 import yaml
+import logging
+
+LOGGER = logging.getLogger(__name__)
 
 class ConfigRuleset(dict):
     defaults = {}
     required = []
-    
+
     def __init__(self):
         self.update(self.defaults)
 
     def validate(self):
         missing = [k for k in self.required if k not in self]
         if missing:
-            raise KeyError("Missing the following keys: {}".format(missing))
+            keyerr = "Missing the following keys: {}".format(missing)
+            logging.critical(keyerr)
+            raise KeyError(keyerr)
+
+    def update_values(self, vals):
+        return self.update(vals)
 
 
 class FileConfigProvider(ConfigRuleset):
@@ -28,11 +42,15 @@ class FileConfigProvider(ConfigRuleset):
             with open(self.source) as f:
                 data = f.read()
         except IOError as err:
-            pass
+            logging.critical("Unable to read file: %s", err)
         return data
 
 
 class YAMLConfigProvider(FileConfigProvider):
+    """
+    YAML implementation of the FileConfigProvider.
+    Assumes a local yaml file called config.yaml
+    """
     def __init__(self, src="config.yaml"):
         FileConfigProvider.__init__(self, src)
 
@@ -44,6 +62,7 @@ class YAMLConfigProvider(FileConfigProvider):
 
 if __name__ == '__main__':
     y = YAMLConfigProvider()
-    y.required = ['name', 'sex']
+    y.required.append('type')
     y.validate()
+    print(y)
     
